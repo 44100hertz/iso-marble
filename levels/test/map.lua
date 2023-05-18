c36f5c9f62a0297135b28ab07ca5240e0e9ed542
Sam Pagenkopf <ssaammp@gmail.com>
2023-05-18 14:49:42 -0500

    add colors to map

--- levels/test/map.lua
+++ levels/test/map.lua
@@ -1,6 +1,10 @@
 return {
   size = {10,10,10},
+  colormap = {
+    purple = {1,0,1,1}
+  },
   objects = {
-    {type='cube', pos = {1,1,1}, size = {3,3,3}}
+    {type='cube', pos = {0,0,0}, size = {3,3,3}, color = "purple"},
+    {type='cube', pos = {0,2,2}, size = {3,3,3}}
   },
 }
