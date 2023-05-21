44c4a81e89bbae4ccfcfe423fa9382f647d2a247
Sam Pagenkopf <ssaammp@gmail.com>
2023-05-21 13:51:25 -0500

    made window resizable

--- conf.lua
+++ conf.lua
@@ -1,10 +1,12 @@
 love.conf = function(t)
    t.gammacorrect = true
    t.title, t.identity = "change-me", "Minimal"
+   t.console = true
    t.modules.joystick = false
    t.modules.physics = false
    t.window.width = 256 * 4
    t.window.height = 144 * 4
    t.window.vsync = true
+   t.window.resizable = true
    t.version = "11.4"
 end
